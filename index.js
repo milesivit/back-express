454bf8ecd4fc40f9727931dee9c2deef959c5972
milena <m.sivit@itecriocuarto.org.ar>
2025-05-24 21:17:44 -0300

    cors para el front

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors'); //cors() habilita que peticiones externas
 
 const app = express()
-
 const port = 3000
+
+app.use(cors());            
+app.use(express.json());
 
 app.use(express.json())
 
