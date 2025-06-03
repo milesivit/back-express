a2e13e8f77fbd6aa99a9a60f525a84ce36a0a86d
milena <m.sivit@itecriocuarto.org.ar>
2025-06-02 22:38:37 -0300

    usuarios y productos en db

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 const path = require('path')
 const filePath = path.join(__dirname, '../data/usuarios.json')
 
+const { Usuario } = require('../models');
+
 const leerUsuario = () => {
     const data = fs.readFileSync(filePath, 'utf8')
     return JSON.parse(data);
@@ -14,47 +16,82 @@ const escribirUsuarios = (usuarios) =>{
     fs.writeFileSync(filePath, JSON.stringify(usuarios, null, 2))
 }
 
-const getUsers = (req, res)=>{
-    res.json({data: usuarios
-        ,status: 200
-        ,message: 'los usuarios han sido obtenidos de manera axitosa'
-    })
-}
-
-const getUserById = (req, res) => {
-    const usuario = usuarios.find(item => item.id === parseInt(req.params.id))
-    console.log(usuario)
-    if (!usuario) return res.json({ status: 400, message: 'usuario no encontrado' })
-        res.json({ data: usuario, status: 200, message: 'usuario encontrado' })
-}
-
-const createUser = (req, res) => {
-    const { nombre, contrasenia, email, edad } = req.body;
-
-    // validaciones basicas
-    if (!email || email.trim() === '') {
-        return res.status(400).json({ status: 400, message: 'El email no puede estar vacío.' });
+const getUsers = async (req, res) => {
+    try {
+      const usuarios = await Usuario.findAll();
+      res.json({
+        data: usuarios,
+        status: 200,
+        message: 'usuarios obtenidos de manera exitosa'
+      });
+    } catch (error) {
+      console.error('Error al obtener usuarios:', error);
+      res.status(500).json({
+        message: error.message || 'Error interno del servidor'
+      });
     }
+  };
 
-    // verificar si el email ya existe 
-    const emailExistente = usuarios.some(user => user.email.toLowerCase() === email.toLowerCase());
-    if (emailExistente) {
-        return res.status(400).json({ status: 400, message: 'El email ya está registrado.' });
+const getUserById = async (req, res) =>{
+    try {
+        const usuario = await Usuario.findByPk(req.params.id)
+        if (!usuario) {
+            return res.status(404).json({ message: 'usuario no encontrado'})
+        } else {
+            res.json({data: usuario, status: 200, message: 'usuario encontrado' })
+        }
+    } catch (error) {
+        console.error('Error al obtener usuario id:', error);
+        res.status(500).json({
+          message: error.message || 'Error interno del servidor'
+        });
     }
+  }
 
-    // crear el nuevo usuario
-    const nuevoUsuario = {
-        id: usuarios.length + 1,
-        nombre,
-        contrasenia,
-        email,
-        edad
-    };
+  const createUser = async (req, res) => {
+    const {nombre, email, edad} = req.body
 
-    usuarios.push(nuevoUsuario);
-    escribirUsuarios(usuarios)
-    res.status(201).json({ status: 201, data: nuevoUsuario, message: 'Usuario creado exitosamente' });
-};
+    try {
+        if (!nombre || !email || !edad) {
+                return res.status(400).json({ message: 'faltan datos obligatorios'})
+        }
+        const nuevoUsuario = await Usuario.create({nombre, email, edad})
+        res.status(201).json({message: 'usuario creado satisfactoriamente', data: nuevoUsuario })
+    } catch (error) {
+        console.error('Error al crear usuario :', error);
+        res.status(500).json({
+          message: error.message || 'Error interno del servidor'
+        });
+    }
+  }
+
+// const createUser = (req, res) => {
+//     const { nombre, contrasenia, email, edad } = req.body;
+
+//     // validaciones basicas
+//     if (!email || email.trim() === '') {
+//         return res.status(400).json({ status: 400, message: 'El email no puede estar vacío.' });
+//     }
+
+//     // verificar si el email ya existe 
+//     const emailExistente = usuarios.some(user => user.email.toLowerCase() === email.toLowerCase());
+//     if (emailExistente) {
+//         return res.status(400).json({ status: 400, message: 'El email ya está registrado.' });
+//     }
+
+//     // crear el nuevo usuario
+//     const nuevoUsuario = {
+//         id: usuarios.length + 1,
+//         nombre,
+//         contrasenia,
+//         email,
+//         edad
+//     };
+
+//     usuarios.push(nuevoUsuario);
+//     escribirUsuarios(usuarios)
+//     res.status(201).json({ status: 201, data: nuevoUsuario, message: 'Usuario creado exitosamente' });
+// };
 
 
 const updateUser = (req, res) =>{
