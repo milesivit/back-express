006581d86132a4d45009e608a67aa87b727ccbf0
milena <m.sivit@itecriocuarto.org.ar>
2025-06-09 22:59:00 -0300

    09-06

--- controllers/venta.controller.js
+++ controllers/venta.controller.js
@@ -3,7 +3,16 @@ const {Venta, Usuario, Producto} = require('../models')
 const getAllSells = async (req, res) =>{
     try {
         const ventas = await Venta.findAll({
-            include:[Usuario, Producto]
+            attributes: ['id', 'total', 'cantidad', 'fecha'],
+            include:[
+                {
+                    model:Usuario, 
+                    attributes:['nombre','email']
+                },
+                {
+                    model:Producto, 
+                    attributes:['nombre','precio']
+                }]
         })
 
         res.json({status:200, data: ventas, message: 'ventas obtenidas de manera exitosa'})
@@ -12,6 +21,72 @@ const getAllSells = async (req, res) =>{
     }
 }
 
+const getSaleById = async (req, res) =>{
+    try {
+        const venta = await Venta.findByPk(req.params.id,{
+            include: [Usuario, Producto]
+        })
+
+        if (!venta) return res.status(404).json({message:'venta no encontrada'})
+        res.json({status:200, data:venta, message:'venta obtenida exitosamente'})
+    } catch (error) {
+      res.status(500).json({message:'error al obtener venta solicitada', error: error})
+    }
+}
+
+const createSale = async (req, res) =>{
+    const {usuarioId, productoId, cantidad, total, fecha} = req.body
+    try {
+        if (!usuarioId || !productoId || !cantidad || !total || !fecha) {
+            return res.status(404).json({message:'faltan campos obligatorios'})
+        }
+
+        const newSale = await Venta.create({usuarioId, productoId, cantidad, total, fecha})
+        res.json({status:201, message:'venta registrada exitosamente', date:newSale})
+    } catch (error) {
+        res.status(500).json({message:'error al crear venta', error: error})
+    }
+}
+
+const updateSale = async (req, res) =>{
+    try {
+        const sale = await Venta.findByPk(req.params.id)
+        if (!sale) return res.status(404).json({message:'venta no encontrada'})
+
+        const {usuarioId, productoId, cantidad, total, fecha} = req.body
+
+        sale.usuarioId = usuarioId || sale.usuarioId
+        sale.productoId = productoId || sale.productoId
+        sale.cantidad = cantidad || sale.cantidad
+        sale.total = total || sale.total
+        sale.fecha = fecha || sale.fecha
+
+        await sale.save()
+        res.json({status:201, message:'venta actualizada exitosamente', date:sale})
+    } catch (error) {
+        res.status(500).json({message:'error al actualizar venta', error: error})
+    }
+}
+
+const deleteSale = async (req, res) =>{
+    try {
+        const sale = await Venta.findByPk(req.params.id);
+        if (!sale) {
+            return res.status(404).json({ status: 404, message: 'venta no encontrada'});
+        }
+  
+        await sale.destroy();
+  
+        res.status(200).json({ status: 200, message: 'venta eliminado exitosamente', date:sale});
+    } catch (error) {
+        res.status(500).json({ status: 500, message: 'Error al eliminar venta', error: error.message });
+    }
+}
+
 module.exports = {
-    getAllSells
+    getAllSells,
+    getSaleById,
+    createSale,
+    updateSale,
+    deleteSale
 }
--- routes/ventas.routes.js
+++ routes/ventas.routes.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {getAllSells} = require('../controllers/venta.controller') 
+const {getAllSells, getSaleById, createSale, deleteSale, updateSale} = require('../controllers/venta.controller') 
 
-router.get('/', getAllSells)
+router.get('/', getAllSells, )
+router.get('/:id', getSaleById)
+router.post('/', createSale)
+router.put('/:id', updateSale)
+router.delete('/:id', deleteSale)
 
 module.exports= router
